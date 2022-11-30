dff826554d2b7b2f471992b4dca9de5fb1a1fbb7
DeK0n <dv.knnv@gmail.com>
2022-11-30 19:54:30 +0200

    commit

---
+++ module_3/t1/1.js
@@ -0,0 +1,3 @@
+const html1 = "<li>First item</li><li>Second item</li><li>Third item</li>";
+document.getElementById("target").innerHTML = html1;
+document.getElementById("target").classList.add("my-list");
---
+++ module_3/t2/2.js
@@ -0,0 +1,3 @@
+const html2 = document.querySelector('#target');
+const list1 = document.createElement('li');
+list1.
